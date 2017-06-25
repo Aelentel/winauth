4224e10082c382ad8676e1cede05ff74b6130e54
Steven Noonan <steven@uplinklabs.net>
2017-06-25 00:30:52 -0700

    version number bump for 3.5.2
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- Authenticator/Properties/AssemblyInfo.cs
+++ Authenticator/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Authenticator implementation")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("WinAuth")]
-[assembly: AssemblyCopyright("Copyright � 2016. Colin Mackie. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2016 Colin Mackie, 2017 Steven Noonan. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.5.0.0")]
-[assembly: AssemblyFileVersion("3.5.0.0")]
+[assembly: AssemblyVersion("3.5.2.0")]
+[assembly: AssemblyFileVersion("3.5.2.0")]
